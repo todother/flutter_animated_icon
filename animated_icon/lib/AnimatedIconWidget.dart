ca5416b8164accc2c9151efc43726a877a9f92cd
geyan <geyan@geyan.local>
2019-09-26 23:19:44 +0800

    first

---
+++ animated_icon/lib/AnimatedIconWidget.dart
@@ -0,0 +1,118 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class AnimatedIconWidget extends StatefulWidget {
+  AnimatedIconWidget(
+      {Key key,
+      @required this.animationList,
+      @required this.icon,
+      @required this.size,
+      this.callback,
+      this.callbackDelay})
+      : super(key: key);
+  final List<IconAnimationStage> animationList;
+  final IconData icon;
+  final VoidCallback callback;
+  final double size;
+  final Duration callbackDelay;
+  _AnimatedIconWidgetState createState() => _AnimatedIconWidgetState();
+}
+
+class _AnimatedIconWidgetState extends State<AnimatedIconWidget>
+    with TickerProviderStateMixin {
+  List<IconAnimationStage> anis = List<IconAnimationStage>();
+  List<AnimationController> controllers = List<AnimationController>();
+  List<Animation<double>> animations = List<Animation<double>>();
+  Animation<double> curAnim;
+  Color curColor;
+  int curIndex = 0;
+  List<bool> ifAdded = List<bool>();
+  double curSize;
+  bool ifInit = true;
+
+  loopAnimation(index) {
+    curColor = curColor == null ? anis.first.color : curColor;
+    if (index < controllers.length - 1) {
+      if (!ifAdded[index]) {
+        animations[index] = animations[index]
+          ..addStatusListener((status) {
+            if (status == AnimationStatus.completed) {
+              curAnim = animations[index + 1];
+              curColor = anis[index + 1].color;
+
+              controllers[index + 1].forward(from: 0);
+              loopAnimation(index + 1);
+            }
+          })
+          ..addListener(() {
+            setState(() {});
+          });
+        ifAdded[index] = true;
+      }
+    } else if (index == controllers.length - 1) {
+      if (!ifAdded[index]) {
+        animations[index] = animations[index]
+          ..addStatusListener((status) {
+            curColor = anis[index].color;
+            if (status == AnimationStatus.completed) {
+              if (widget.callback != null) {
+                if (widget.callbackDelay == null) {
+                  widget.callback();
+                } else {
+                  Timer(widget.callbackDelay, () {
+                    widget.callback();
+                  });
+                }
+              }
+            }
+          })
+          ..addListener(() {
+            setState(() {});
+          });
+        ifAdded[index] = true;
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    anis = widget.animationList;
+
+    List.generate(anis.length, (index) {
+      var curAni = anis[index];
+      ifAdded.add(false);
+      controllers.add(
+          AnimationController(vsync: this, duration: anis[index].duration));
+      animations.add(Tween(begin: curAni.start, end: curAni.end)
+          .animate(controllers[index]));
+    });
+    curAnim = animations.first;
+    loopAnimation(0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      padding: EdgeInsets.all(0),
+      onPressed: () {
+        controllers.first.forward(from: 0);
+      },
+      icon: Icon(
+        widget.icon,
+        size: widget.size * curAnim.value,
+        color: curColor,
+      ),
+    );
+  }
+}
+
+class IconAnimationStage {
+  double start;
+  double end;
+  Color color;
+  Duration duration;
+
+  IconAnimationStage({this.color, this.duration, this.end, this.start});
+}
---
+++ animated_icon/lib/main.dart
@@ -0,0 +1,132 @@
+import 'package:animated_icon/AnimatedIconWidget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("AnimatedIcon"),
+          
+        ),
+        body:Center(child:Home()) ,
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: getButtonList(context),
+    );
+  }
+}
+
+getButtonList( BuildContext context) {
+  List<IconAnimationStage> stages1 = List<IconAnimationStage>();
+  stages1.add(IconAnimationStage(
+      color: Colors.grey[500],
+      start: 1.0,
+      end: 0.0,
+      duration: Duration(milliseconds: 200)));
+  stages1.add(IconAnimationStage(
+      color: Colors.redAccent,
+      start: 0.0,
+      end: 1.3,
+      duration: Duration(milliseconds: 300)));
+  stages1.add(IconAnimationStage(
+      color: Colors.redAccent,
+      start: 1.3,
+      end: 1.0,
+      duration: Duration(milliseconds: 100)));
+
+  List<IconAnimationStage> stages2 = List<IconAnimationStage>();
+  stages2.add(IconAnimationStage(
+      color: Colors.grey[500],
+      start: 1.0,
+      end: 1.2,
+      duration: Duration(milliseconds: 200)));
+  stages2.add(IconAnimationStage(
+      color: Colors.grey[500],
+      start: 1.2,
+      end: 1.0,
+      duration: Duration(milliseconds: 200)));
+
+  List<IconAnimationStage> stages3 = List<IconAnimationStage>();
+  stages3.add(IconAnimationStage(
+      color: Colors.redAccent,
+      start: 1.0,
+      end: 1.2,
+      duration: Duration(milliseconds: 200)));
+  stages3.add(IconAnimationStage(
+      color: Colors.grey[500],
+      start: 1.2,
+      end: 1.0,
+      duration: Duration(milliseconds: 200)));
+  double iconSize = 70 ;
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.spaceAround,
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: <Widget>[
+      AnimatedIconWidget(
+          key: UniqueKey(),
+          animationList: stages1,
+          icon: Icons.favorite,
+          size: iconSize,
+          callback: () {
+            print("clicked");
+          }),
+      AnimatedIconWidget(
+        key: UniqueKey(),
+        animationList: stages3,
+        icon: Icons.favorite,
+        size: iconSize,
+        callback: () {
+          print("clicked");
+        },
+      ),
+      AnimatedIconWidget(
+        animationList: stages2,
+        icon: Icons.comment,
+        size: iconSize,
+        callbackDelay: Duration(milliseconds: 200),
+        callback: () {
+          showBottom(context);
+        },
+      ),
+      AnimatedIconWidget(
+        animationList: stages2,
+        icon: Icons.reply,
+        size: iconSize,
+      ),
+    ],
+  );
+}
+
+showBottom(context) {
+  showModalBottomSheet(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusDirectional.circular(10)),
+      context: context,
+      builder: (_) {
+        return Container(
+          height: 600,
+          child: GestureDetector(
+              onTap: () {
+                FocusScope.of(context).requestFocus(FocusNode());
+              },
+              child: Container()),
+        );
+      });
+}
